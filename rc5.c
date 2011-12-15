4f927e79bb4d8b234af98f7d71cb11e2230b84fd
David Nieder <davidnieder@gmx.de>
2011-12-15 19:55:35 +0100

    rc5 rewrite

--- rc5.c
+++ rc5.c
@@ -1,3 +1,10 @@
+/*
+ * Modified version from
+ * Peter Daneger's rc5 decoder:
+ *
+ * http://www.mikrocontroller.net/topic/12216
+ */
+
 #ifndef RC5_C
 #define RC5_C
 
@@ -8,8 +15,6 @@ void rc5_init(void)
     /* initialze timer/counter 0 */
     TCCR0 |= (1<<CS02);    /* prescaler 256 */
     TIMSK |= (1<<TOIE0);   /* enable timer/counter0 overflow interrupt */
-
-    count = 0;
 }
 
 void rc5_handler(void)
@@ -19,32 +24,30 @@ void rc5_handler(void)
 
 ISR (TIMER0_OVF_vect)
 {
-  uint16_t tmp = rc5_tmp;				// for faster access
-  TCNT0 = -2;					// 2 * 256 = 512 cycle
+    TCNT0 = -2;					                            /* 2*256 = 512 cycle */
 
-  if( ++rc5_time > PULSE_MAX ){			// count pulse time
-    if( !(tmp & 0x4000) && tmp & 0x2000 )	// only if 14 bits received
-      rc5_data = tmp;
-    tmp = 0;
-  }
-
-  if( (rc5_bit ^ xRC5_IN) & 1<<xRC5 ){		// change detect
-    rc5_bit = ~rc5_bit;				// 0x00 -> 0xFF -> 0x00
-
-    if( rc5_time < PULSE_MIN )			// to short
-      tmp = 0; 
-
-    if( !tmp || rc5_time > PULSE_1_2 ){		// start or long pulse time
-      if( !(tmp & 0x4000) )			// not to many bits
-        tmp <<= 1;				// shift
-      if( !(rc5_bit & 1<<xRC5) )		// inverted bit
-        tmp |= 1;				// insert new bit
-      rc5_time = 0;				// count next pulse time
+    if( ++rc5_time > PULSE_MAX )    {			            /* pulse time in range? */
+        if( !(rc5_tmp & 0x4000) && (rc5_tmp & 0x2000) )	    /* 14 bits received? */
+            rc5_data = rc5_tmp;
+            rc5_tmp = 0;
     }
-  }
 
-  rc5_tmp = tmp;
+    if( (rc5_bit ^ IRPIN) & (1<<IRLED) )  {		            /* change detected? */
+        rc5_bit = ~rc5_bit;				                    /* invert bits */
 
+        if( rc5_time < PULSE_MIN )			                /* pulse to short? */
+            rc5_tmp = 0;
+
+        if( !rc5_tmp || rc5_time > PULSE_1_2 )  {		    /* start or long pulse time */
+
+            if( !(rc5_tmp & 0x4000) )			            /* not to many bits */
+                rc5_tmp <<= 1;				                /* shift */
+
+            if( !(rc5_bit & 1<<IRLED) )		                /* inverted bit */
+                rc5_tmp |= 1;				                /* insert new bit */
+                rc5_time = 0;				                /* count next pulse time */
+        }
+    }
 }
 
 #endif
--- rc5.h
+++ rc5.h
@@ -5,21 +5,25 @@
     #error rc5.h: F_CPU not defined
 #endif
 
-#define xRC5_IN     IRPIN
-#define xRC5        IRLED     // IR input low active
+#ifndef IRPIN
+    #error rc5.h: IRPIN not defined
+#endif
 
-#define RC5TIME     1.778e-3        // 1.778msec
-#define PULSE_MIN   (unsigned char)(F_CPU / 512 * RC5TIME * 0.4 + 0.5)
-#define PULSE_1_2   (unsigned char)(F_CPU / 512 * RC5TIME * 0.8 + 0.5)
-#define PULSE_MAX   (unsigned char)(F_CPU / 512 * RC5TIME * 1.2 + 0.5)
+#ifndef IRLED
+    #error rc5.h: IRLED not defined
+#endif
 
 
-uint8_t  rc5_bit;                // bit value
-uint8_t  rc5_time;               // count bit time
-uint16_t rc5_tmp;                // shift bits in
-int16_t  rc5_data;               // store result
+#define RC5TIME     1.778e-3        /* 1.778msec */
+#define PULSE_MIN   (uint8_t)(F_CPU / 512 * RC5TIME * 0.4 + 0.5)
+#define PULSE_1_2   (uint8_t)(F_CPU / 512 * RC5TIME * 0.8 + 0.5)
+#define PULSE_MAX   (uint8_t)(F_CPU / 512 * RC5TIME * 1.2 + 0.5)
 
-uint8_t  count;
+
+uint8_t  rc5_bit;       /* bit value */
+uint8_t  rc5_time;      /* measure bit time */
+uint16_t rc5_tmp;       /* temporary value */
+int16_t  rc5_data;      /* store result */
 
 /* prototypes */
 void rc5_init(void);
